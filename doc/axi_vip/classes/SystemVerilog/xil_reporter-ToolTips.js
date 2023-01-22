9c67e692f382b32a34a1e492f61b0616ceeb2a5a
Pedro Gimenes <pgimenes@outlook.com>
2023-01-22 19:47:53 +0000

    axi_vip documentation

---
+++ doc/axi_vip/classes/SystemVerilog/xil_reporter-ToolTips.js
@@ -0,0 +1,1 @@
+NDContentPage.OnToolTipsLoaded({93:"<div class=\"NDToolTip TClass LSystemVerilog\"><div class=\"TTSummary\">The xil_reporter class is a class which extends from xil_object and add its own get_type_name function</div></div>"});
