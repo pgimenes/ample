85247332c6804d24b8226f0de025956509f387f0
Pedro Gimenes <pg519@ee-beholder0.ee.ic.ac.uk>
2023-01-24 18:12:52 +0000

    removed old

--- fuzzy_carnival_base/ip/transformation_engine/fv/jgproject/.tmp/.initCmds.tcl
+++
@@ -1,3 +0,0 @@
-@puts "INFO: successfully checked out licenses \"jasper_interactive\" and \"jasper_fao\"."
-@puts "INFO: reading configuration file \"/home/pg519/.config/jasper/jaspergold.conf\"."
-@get_message -clear
