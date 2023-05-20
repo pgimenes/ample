126af2af1271c55a41dcca5130224e3537f2200b
Pedro Gimenes <pgimenes@outlook.com>
2023-05-20 14:52:32 +0100

    fixed regbank update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,23 +7,23 @@ exec > build_log.log
 cd $FYP_DIR
 
 # Build submodules
-echo "====================================="
+echo "======================================================="
 echo "[$(date +%Y-%m-%d\ %H:%M:%S)]: Building submodules."
-echo "====================================="
+echo "======================================================="
 
 git submodule update --init --recursive
 git submodule foreach git pull origin master
 
-# Build register banks
-echo "====================================="
-echo "[$(date +%Y-%m-%d\ %H:%M:%S)]: Building register banks."
-echo "====================================="
-
-python3.6 $FYP_DIR/scripts/update_regbanks.py
-
 # Build Xilinx IP
-echo "====================================="
+echo "======================================================="
 echo "[$(date +%Y-%m-%d\ %H:%M:%S)]: Building Xilinx IP."
-echo "====================================="
+echo "======================================================="
 
 vivado -mode batch -source $FYP_DIR/scripts/generate_ip.tcl
+
+# Build register banks
+echo "======================================================="
+echo "[$(date +%Y-%m-%d\ %H:%M:%S)]: Building register banks."
+echo "======================================================="
+
+python3.6 $FYP_DIR/scripts/update_regbanks.py
