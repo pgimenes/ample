b27fecdc7ccd3fb2cbdfce488fd2b96f8cf060e4
Pedro Gimenes <pgimenes@outlook.com>
2023-06-15 11:23:10 +0100

    synth script

--- scripts/synth.tcl
+++ scripts/synth.tcl
@@ -1,3 +1,7 @@
-# Synthesis
-# create_clock -period 20.000 -name regbank_clk -waveform {0.000 10.000} [get_ports {regbank_clk}]
-# create_clock -period 5.000 -name sys_clk -waveform {0.000 2.500} [get_ports {sys_clk}]
+# Launch synthesis
+launch_runs synth_1 -jobs 40
+wait_on_run synth_1 -quiet
+
+# Open design and generate timing report
+open_run synth_1 -name synth_1
+report_timing_summary -delay_type min_max -report_unconstrained -check_timing_verbose -max_paths 10 -input_pins -routable_nets -name timing_1
