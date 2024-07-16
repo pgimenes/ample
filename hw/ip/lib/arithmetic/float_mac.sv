aaa43006dc8c6ab4f0b84190dd3c2da58158c3f6
alexfrater <whitealez@hotmail.com>
2024-07-16 10:31:13 +0100

    fast multiplication bug fix

--- hw/ip/lib/arithmetic/float_mac.sv
+++ hw/ip/lib/arithmetic/float_mac.sv
@@ -62,16 +62,16 @@ logic                   busy;
 
 `ifdef SIMULATION_QUICK
 
-assign fp_mult_result_valid_comb = in_valid && in_ready;
+assign fp_mult_result_valid_comb = in_valid; //&& in_ready;
 assign fp_mult_result_comb = a;
-assign fp_add_result_valid_comb = busy && fp_mult_result_valid_q;
+assign fp_add_result_valid_comb = fp_mult_result_valid_q;
 assign fp_add_result_comb = acc_reg;
 
 `else
 logic [FLOAT_WIDTH-1:0] a_gated;
 logic [FLOAT_WIDTH-1:0] b_gated;
 
-logic [FLOAT_WIDTH-1:0] fp_mult_result_q_gated;
+// logic [FLOAT_WIDTH-1:0] fp_mult_result_q_gated;
 logic [FLOAT_WIDTH-1:0] acc_reg_gated;
 
 
@@ -79,25 +79,25 @@ logic [FLOAT_WIDTH-1:0] acc_reg_gated;
 assign a_gated = in_valid ? a : '0;
 assign b_gated = in_valid ? b : '0;
 
-assign fp_mult_result_q_gated = fp_mult_result_valid_q ? fp_mult_result_q : '0;
+// assign fp_mult_result_q_gated = fp_mult_result_valid_q ? fp_mult_result_q : '0;
 
 fp_mult multiplier_i (
 //   .s_axis_a_tvalid(in_valid && in_ready),
-  .in1(a),
+  .in1(a_gated),
 
 //   .s_axis_b_tvalid(in_valid && in_ready),
-  .in2(b),
+  .in2(b_gated),
   
 //   .m_axis_result_tvalid(fp_mult_result_valid_comb),
   .res(fp_mult_result_comb)
 );
-assign fp_mult_result_valid_comb = in_valid && in_ready;
+assign fp_mult_result_valid_comb = in_valid; //&& in_ready;
 
 fp_add adder_i (
 //   .s_axis_a_tvalid              (busy && fp_mult_result_valid_q),
 
 
-  .in1               (fp_mult_result_q_gated),
+  .in1               (fp_mult_result_q),
   
 //   .s_axis_b_tvalid              (busy && fp_mult_result_valid_q),
   .in2               (acc_reg),
@@ -105,7 +105,7 @@ fp_add adder_i (
 //   .m_axis_result_tvalid         (fp_add_result_valid_comb),
   .res          (fp_add_result_comb)
 );
-assign fp_add_result_valid_comb = busy && fp_mult_result_valid_q;
+assign fp_add_result_valid_comb =  fp_mult_result_valid_q;
 
 `endif
 
@@ -124,13 +124,13 @@ always_ff @(posedge core_clk or negedge resetn) begin
         fp_mult_result_q       <= '0;
 
         fp_add_result_valid <= '0;
-        fp_add_result <= '0;
+        // fp_add_result <= '0;
     end else begin
         fp_mult_result_valid_q <= fp_mult_result_valid_comb;
         fp_mult_result_q       <= fp_mult_result_comb;
 
         fp_add_result_valid <= fp_add_result_valid_comb;
-        fp_add_result <= fp_add_result_comb;
+        // fp_add_result <= fp_add_result_comb;
     end
 end
 
@@ -142,31 +142,31 @@ always_ff @(posedge core_clk or negedge resetn) begin
         acc_reg <= '0;
     end else begin
         acc_reg <= overwrite ? overwrite_data
-                    : busy && fp_add_result_valid ? fp_add_result 
+                    : fp_add_result_valid_comb ? fp_add_result_comb
                     : acc_reg;
     end
 end
 
 assign accumulator = acc_reg;
 
-// Handle backpressure
-// -----------------------------------
+// // Handle backpressure
+// // -----------------------------------
 
-always_ff @(posedge core_clk or negedge resetn) begin
-    if (!resetn) begin
-        busy <= '0;
+// always_ff @(posedge core_clk or negedge resetn) begin
+//     if (!resetn) begin
+//         busy <= '0;
 
-    end else begin
-        busy <= 
-                // Accepting new update request
-                in_valid && in_ready ? 1'b1
+//     end else begin
+//         busy <= 
+//                 // Accepting new update request
+//                 in_valid && in_ready ? 1'b1
                 
-                // Done with update request
-                : busy && fp_add_result_valid ? 1'b0
+//                 // Done with update request
+//                 : busy && fp_add_result_valid ? 1'b0
 
-                : busy;
-    end
-end
+//                 : busy;
+//     end
+// end
 
 assign in_ready = !busy;
 
