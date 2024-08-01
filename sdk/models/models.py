02f5d87a776937f0d401dacbc8e44a552c7f1b4d
alexfrater <whitealez@hotmail.com>
2024-08-01 18:02:48 +0100

    edge embed model

--- sdk/models/models.py
+++ sdk/models/models.py
@@ -141,7 +141,7 @@ Graph Convolutional Network with attatched linear layers
 '''
 
 class GCN_MLP_Model(nn.Module):
-    def __init__(self, in_channels, out_channels, layer_count=1, hidden_dimension=34,precision = torch.float32):
+    def __init__(self, in_channels, out_channels, layer_count=1, hidden_dimension=32,precision = torch.float32):
         super().__init__()
         self.precision = precision
         self.layers = nn.ModuleList()
@@ -213,7 +213,7 @@ class MLP_Model(torch.nn.Module):
             layer.to(self.precision)
 
     def forward(self, x, edge_index,):
-        x = x.to(self.precision)  
+        # x = x.to(self.precision)  
         outputs = []
         for layer in self.layers:
             x = layer(x)
@@ -223,102 +223,100 @@ class MLP_Model(torch.nn.Module):
 
 
 
+def convert_edges_to_nodes(edge_index):
+    num_edges = edge_index.size(1)
+    num_nodes = edge_index.max().item() + 1  # Number of original nodes
+    
+    # New node indices for each edge
+    new_node_indices = torch.arange(num_nodes, num_nodes + num_edges)
+    
+    # Source nodes for the new edges
+    src_nodes = edge_index[0]
+    # Destination nodes for the new edges
+    dst_nodes = edge_index[1]
+    
+    # New edges: connect source node to new node and new node to destination node
+    new_edges_1 = torch.stack([src_nodes, new_node_indices])
+    new_edges_2 = torch.stack([new_node_indices, dst_nodes])
+    
+    # Concatenate the new edges to form the new edge_index
+    new_edge_index = torch.cat([new_edges_1, new_edges_2], dim=1)
+    
+    return new_edge_index
 
-class Edge_ConcatEmbedding_Model(torch.nn.Module): #NodeRx_Src_Embedding_Model
+
+class EdgeGCNLayer(nn.Module):
+    def __init__(self, in_channels=32, out_channels=32):
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.update_mlp = nn.Linear(in_channels, out_channels)
+
+    def forward(self, src_embed, rx_embed, edge_embed):
+        # Aggregating the embeddings with learnable weights
+        combined = src_embed + rx_embed + edge_embed
+
+        # Update with a non-linearity and another MLP layer
+        updated_edge_embed = self.update_mlp(combined)
+        
+        return updated_edge_embed
+
+
+torch.fx.wrap('agg_mlp')
+
+def agg_mlp(x1, x2, x3):
+    model = nn.Sequential(
+    nn.Linear(32, 32), 
+    )
+    output = model(x1 + x2 + x3)
+
+    return output
+
+class Edge_Embedding_Model(torch.nn.Module): #NodeRx_Src_Embedding_Model
     def __init__(self, in_channels, out_channels=32, layer_count=1, hidden_dimension=32, precision = torch.float32):
         super().__init__()
         self.precision = precision
         self.layers = nn.ModuleList()
 
         #########Source Node MLP#########
-        layer = nn.Linear(in_channels, out_channels, bias=True)
-        layer.name = 'input_output_layer_src'  # Assign name directly
-        self.layers.append(layer)
-
+        self.src_embedder = nn.Linear(in_channels, out_channels, bias=True)
 
         #########Receive Node MLP#########
-        layer = nn.Linear(in_channels, out_channels, bias=True)
-        layer.name = 'input_output_layer_rx'  # Assign name directly
-        self.layers.append(layer)
+        self.rx_embedder = nn.Linear(in_channels, out_channels, bias=True)
+        # layer.name = 'input_output_layer_rx'  # Assign name directly
+        # self.layers.append(layer)
 
 
         #########Edge Node MLP#########
-        layer = nn.Linear(in_channels, hidden_dimension, bias=True)
-        layer.name = 'input_layer_edge'  # Assign name directly
-        self.layers.append(layer)
+        #Change to GCN to aggregate itself and last layer edge if not first model
+        self.edge_embedder = nn.Linear(in_channels, hidden_dimension, bias=True)
+        # layer.name = 'input_layer_edge'  # Assign name directly
+        # self.layers.append(layer)
 
-        #########Edge Update GCN#########
-        layer = GCNConv(hidden_dimension, out_channels,normalize =False)
-        layer.name = f'gcn_layer_output_edge{layer_count-1}'
-        self.layers.append(layer)
-       
 
         for layer in self.layers:
             layer.to(self.precision)
 
-    def forward(self, x, edge_index,):
-        x = x.to(self.precision)  
-        src_embed = x
-        rx_embed = x
+    def forward(self, data, edge_index):
+        # x = x.to(self.precision)  
         outputs = []
-        for layer in self.layers:
-            if 'src' in layer.name:
-                src_embed = layer(src_embed)
-                outputs.append(src_embed)
+        #Somehow need to choose x[col] and x[row], perhaps give each their own nodeslots?
+        x= data.x
+        edge_attr = data.edge_attr
+        row, col = edge_index
 
-            if 'rx' in layer.name:
-                rx_embed = layer(rx_embed)
-                outputs.append(rx_embed)
+        src_embed = self.src_embedder(x[col])
+        outputs.append(src_embed)
 
-            # x = layer(x)
-            # outputs.append(x)
+        rx_embed = self.rx_embedder(x[row])
+        outputs.append(rx_embed)
+
+        edge_embed = self.edge_embedder(edge_attr)
+        outputs.append(edge_embed)
+
+        edge_update = agg_mlp(src_embed,rx_embed,edge_embed)
+
+        outputs.append(edge_update)
 
         return outputs
-
-
-#Skeleton Code
-class Edge_Update_Model(torch.nn.Module):
-    def __init__(self, in_features=32, out_features=32, layer_count=2, hidden_dimension=32, precision = torch.float32):
-        super().__init__()
-
-        self.node_embedder =  NodeRx_Src_Embedding_Model(in_features, out_features, layer_count, hidden_dimension, precision)
-
-        self.edge_embedder = Edge_Embedding_Model(in_features, out_features, layer_count, hidden_dimension, precision)
-
-
-        def forward(self, x, edge_index, edge_attr):
-            outputs = []
-            x = x.to(self.precision)  
-            src,rx = self.node_embedder(x, edge_index)
-            edge_embed = self.edge_embedder(x)
-
-            for layer in self.layers:
-                x = layer(x)
-                outputs.append(x)
-            return outputs
-
-
-
-
-# class Edge_Embedding_Model(torch.nn.Module):
-#     def __init__(self, in_features=32, out_features=32, layer_count=2, hidden_dimension=32, precision = torch.float32):
-#         super().__init__()
-
-     
-
-#         #########Edge Embedding MLP#########
-#         if layer_count == 1:
-           
-
-#         def forward(self, x, edge_index, edge_attr):
-#             outputs = []
-#             x = x.to(self.precision)  
-#             edge_embed = x
-#             for layer in self.layers:
-                
-#                 x = layer(x)
-#                 outputs.append(x)
-
-#             return outputs
-
 
